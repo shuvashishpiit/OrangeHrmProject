eae884407c5d75a413255f431e5686a3cde9ed48
shuvashish <shuvashish@piit.us>
2022-08-23 22:06:30 -0400

    Added one test

---
+++ OrangeHrmProject/src/main/java/pages/LoginPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import browser.Browser;
+import browser.Info;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(css = "input[name='username']") private WebElement userName;
+    @FindBy(xpath = "//input[@name='password']") private WebElement password;
+    @FindBy(className = "orangehrm-login-button") private WebElement loginButton;
+
+    public PIMPage loginToPIMPage() {
+        userName.sendKeys(Info.USER_NAME);
+        password.sendKeys(Info.PASSWORD);
+        loginButton.click();
+        return PageFactory.initElements(Browser.driver, PIMPage.class);
+    }
+
+
+}
